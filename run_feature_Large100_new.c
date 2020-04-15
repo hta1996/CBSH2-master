dc30ed032f94c2b6203978d90e9c6a1463e127e9
hta1996 <huangtaoan@163.com>
2020-04-14 18:19:11 -0700

    cas

---
+++ run_feature_Large100_new.c
@@ -0,0 +1,97 @@
+#include "mpi.h"
+#include <string.h>
+int main (int argc, char *argv[])
+{
+    int i, rank, size, namelen;
+    char name [MPI_MAX_PROCESSOR_NAME];
+
+    MPI_Init (&argc, &argv);
+
+    MPI_Comm_size (MPI_COMM_WORLD, &size);
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+    MPI_Get_processor_name (name, &namelen);
+
+    char str1[300]=" ./CBSH2 -m instances/lak503d.map -o tmplarge100_new.csv -t 7200 -s 1 -h WDG -a instances/lak503dmap-100agents-";
+    char str2[300]=".agents -u 1 --collect 1 --feature featureLarge100/featureLarge100new2_";
+    char str3[300]=".txt";
+    char strnum[300];
+    printf ("Running from task %d on %s!\n", rank, name);
+    sprintf(strnum, "%d", rank);
+    char command[300];
+    char sleep[300]="sleep ";
+    memset(command,0,sizeof(command));
+    strcat(command,str1);
+    strcat(command,strnum);
+    strcat(command,str2);
+    strcat(command,strnum);
+    strcat(command,str3);
+    strcat(sleep,strnum);
+    system(sleep);
+    system(command);
+    
+    if (rank == 0 )
+       printf ("MPI World size = %d processes\n", size);
+
+    MPI_Finalize ();
+
+}
+/*
+ ./CBSH2 -m instances/lak503d.map -o tmplarge100_new.csv -t 7200 -s 1 -h WDG -a instances/lak503dmap-100agents-0.agents -u 1 --collect 1 --feature featureLarge100/featureLarge100new_0.txt
+
+./CBSH2 -m instances/lak503d.map -o tp_new.csv -t 3700 -s 1 -h WDG -a instances/lak503dmap-100agents-0.agents -u 4 --model featureLarge100/large_comb
+ #./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-10.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-11.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-12.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-13.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-14.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-15.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-16.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-17.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-18.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-19.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-20.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-21.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-22.agents -u 4
+#./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-23.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-42.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-43.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-44.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-45.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-46.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-47.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-48.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-49.agents -u 4
+sleep 72000
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-24.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-25.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-26.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-27.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-28.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-29.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-30.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-31.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-32.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-33.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-34.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-35.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-36.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-37.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-38.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-39.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-40.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-41.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-42.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-43.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-44.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-45.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-46.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-47.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-48.agents -u 4
+./CBSH_Large_ML3 -m instances/lak503d.map -o runML_large_100_4.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-49.agents -u 4
+
+
+
+
+./CBSH_Large_ML3 -m instances/lak503d.map -o tp.csv -t 5400 -s 1 -h WDG -a instances/lak503dmap-100agents-22.agents -u 4
+
+*/
